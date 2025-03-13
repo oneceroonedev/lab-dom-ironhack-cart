b16646e06c5501e7d9ccb9fb39ab1b253c46941d
oneceroonedev <oneceroonedev@gmail.com>
2025-03-13 17:46:41 +0100

    W7-D3-LAB1

--- src/index.js
+++ src/index.js
@@ -1,42 +1,99 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  const price = parseFloat(product.querySelector('.price span').textContent);
+  let quantity = Number(product.querySelector('.quantity input').value);
+  let subtotal = price * quantity;
 
-  //... your code goes here
+  let subtotalResult = product.querySelector('.subtotal span');
+  subtotalResult.textContent = subtotal;
+
+  return subtotal;
 }
 
 function calculateAll() {
-  // code in the following two lines is added just for testing purposes.
-  // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
-  // end of test
+  const products = document.querySelectorAll('.product');
 
-  // ITERATION 2
-  //... your code goes here
+  let total = 0;
 
-  // ITERATION 3
-  //... your code goes here
+  products.forEach(product => {
+    total += updateSubtotal(product);
+  });
+
+  let totalResult = document.querySelector('#total-value span');
+  totalResult.textContent = total;
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
-  //... your code goes here
+
+  const product = target.closest('.product');
+  product.remove();
+
+  //We recalculate total of cart
+  calculateAll();
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+
+  const createProductLine = document.querySelector('.create-product');
+
+  let productNameInput = createProductLine.querySelector('input[type="text"]');
+  let productPriceInput = createProductLine.querySelector('input[type="number"]');
+
+  let productName = productNameInput.value;
+  let productPrice = productPriceInput.value;
+
+  // We validate that the product name field is not empty
+  if (!productName) {
+    return alert('Por favor, introduce un nombre al producto.');
+  }
+
+  const newProduct = document.createElement('tr');
+  newProduct.className = 'product';
+
+  newProduct.innerHTML = `
+    <td class="name">
+      <span>${productName}</span>
+    </td>
+    <td class="price">$<span>${productPrice}</span></td>
+    <td class="quantity">
+      <input type="number" value="0" min="0" placeholder="Quantity" />
+    </td>
+    <td class="subtotal">$<span>0</span></td>
+    <td class="action">
+      <button class="btn btn-remove">Remove</button>
+    </td>
+  `;
+  
+  const spaceNewProduct = document.querySelector('#cart tbody');
+  spaceNewProduct.appendChild(newProduct);
+
+  // We added the event to the remove button to make it work
+  const removeBtn = newProduct.querySelector('.btn-remove');
+  removeBtn.addEventListener('click', removeProduct);
+
+  // Clear inputs once the new product has been added
+  productNameInput.value = '';
+  productPriceInput.value = 0;
+
+  return newProduct;
 }
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+  const removeBtn =  document.getElementsByClassName('btn-remove');
+  for (let i = 0; i < removeBtn.length; i++) {
+    const button = removeBtn[i];
+    button.addEventListener('click', removeProduct);
+  }
+
+  const createProductBtn = document.getElementById('create');
+  createProductBtn.addEventListener('click', createProduct);
 });
